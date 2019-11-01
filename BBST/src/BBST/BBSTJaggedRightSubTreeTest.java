75b015a5fe3a24f730f13eac603c8012d2c7e48a
JudeaDumont <dumontjudea2@gmail.com>
2019-11-01 01:41:11 -0600

    Removed irrelevant comment

--- BBST/src/BBST/BBSTJaggedRightSubTreeTest.java
+++ BBST/src/BBST/BBSTJaggedRightSubTreeTest.java
@@ -2,8 +2,6 @@ package BBST;
 
 public class BBSTJaggedRightSubTreeTest {
     public static void main(String[] args) throws Exception {
-        //Plan to get this test passing next.
-        //This test does not currently pass.
         BBST<Integer> bbst = new BBST<Integer>();
         bbst.add(8);
         bbst.add(16);
